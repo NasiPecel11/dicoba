02073be909a870143d168893810ae82c27c4b7a4
SaosSambel <galihprakoso126@gmail.com>
2022-03-08 01:04:58 +0800

    luxspace

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      spacing: {
+        '13': '3.25rem',
+      },
+
+      fontFamily: {
+        inter : [ 'Inter' ],
+      },
+
+      colors:{
+
+        
+
+      },
+    },
+  },
+  plugins: [],
+}
